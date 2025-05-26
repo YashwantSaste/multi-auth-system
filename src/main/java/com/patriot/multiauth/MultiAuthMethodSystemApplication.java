15220f90008afbd1bb76d06c93d7b91b2c6846f7
Yashwant Saste <yashwant.saste.ext@siemens.com>
2025-05-26 23:34:05 +0530

    Initial commit - Project skeleton and basic pom.xml

---
+++ src/main/java/com/patriot/multiauth/MultiAuthMethodSystemApplication.java
@@ -0,0 +1,13 @@
+package com.patriot.multiauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiAuthMethodSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiAuthMethodSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/patriot/multiauth/MultiAuthMethodSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.patriot.multiauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiAuthMethodSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
